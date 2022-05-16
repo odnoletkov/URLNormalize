d2d1e4e0c10543f7cdb2b0316f7efdd6d43e57b4
Maksim Odnoletkov <odnoletkov@mail.ru>
2022-05-16 23:07:52 +0100

    Normalize percent encodings

--- Tests/URLNormalizeTests/URLNormalizeTests.swift
+++ Tests/URLNormalizeTests/URLNormalizeTests.swift
@@ -2,10 +2,41 @@ import XCTest
 @testable import URLNormalize
 
 final class URLNormalizeTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(URLNormalize().text, "Hello, World!")
+
+    /// https://en.wikipedia.org/wiki/URI_normalization
+    func testWiki() throws {
+
+        // Converting percent-encoded triplets to uppercase
+        XCTAssertEqual(
+            URL(string: "http://example.com/foo%2a")!.normalized!.absoluteString,
+            "http://example.com/foo*"
+        )
+        XCTAssertEqual(
+            URL(string: "http://us%2fer:pass%2fword@ex%2fample.com/foo%3b?qu%5eery#frag%5ement")!.normalized!.absoluteString,
+            "http://us%2Fer:pass%2Fword@ex%2Fample.com/foo%3B?qu%5Eery#frag%5Ement"
+        )
+
+
+    }
+}
+
+extension URL {
+    var normalized: URL? {
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: false) else {
+            return nil
+        }
+        components.normalizePercentEncodings()
+        return components.url
+    }
+}
+
+extension URLComponents {
+    mutating func normalizePercentEncodings() {
+        user = user
+        password = password
+        host = host
+        path = path
+        query = query
+        fragment = fragment
     }
 }
