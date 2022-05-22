c5f48474cdaa4e20f1c8e7422ee12e9ef9ca1548
Maksim Odnoletkov <odnoletkov@mail.ru>
2022-05-22 22:30:58 +0100

    Make all options public

--- Sources/URLNormalize/URLNormalize.swift
+++ Sources/URLNormalize/URLNormalize.swift
@@ -12,6 +12,93 @@ public extension URLComponents {
     }
 }
 
+public extension URLComponents.Normalization {
+    /// Normalizations that preserve semantics:
+    
+    /// Converting percent-encoded triplets to uppercase.
+    /// Decoding percent-encoded triplets of unreserved characters.
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let percentEncodings = Self(rawValue: 1 << 0)
+    
+    /// Converting the scheme and host to lowercase.
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let lowercaseSchemeAndHost = Self(rawValue: 1 << 1)
+    
+    /// Removing dot-segments.
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let removeDotSegments = Self(rawValue: 1 << 2)
+    
+    /// Converting an empty path to a "/" path.
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let convertEmptyPath = Self(rawValue: 1 << 3)
+    
+    /// Removing the default port.
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let removeDefaultPort = Self(rawValue: 1 << 4)
+    
+    
+    /// Normalizations that usually preserve semantics:
+    
+    /// Adding a trailing "/" to a non-empty path.
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let addTrailingSlash = Self(rawValue: 1 << 5)
+    
+    
+    /// Normalizations that change semantics:
+    
+    /// Removing the fragment.
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let removeFragment = Self(rawValue: 1 << 6)
+    
+    /// Removing duplicate slashes.
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let removeDuplicateSlashes = Self(rawValue: 1 << 7)
+    
+    /// Sorting the query parameters.
+    /// Not guaranteed to be stable!
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let sortQueryParameters = Self(rawValue: 1 << 8)
+    
+    /// Removing the "?" when the query is empty.
+    /// From https://en.wikipedia.org/wiki/URI_normalization
+    static let removeEmptyQuery = Self(rawValue: 1 << 9)
+    
+
+    /// JS normalizations:
+    
+    /// Force HTTP
+    /// From https://github.com/sindresorhus/normalize-url
+    static let forceHTTP = Self(rawValue: 1 << 10)
+    
+    /// Force HTTP
+    /// From https://github.com/sindresorhus/normalize-url
+    static let forceHTTPS = Self(rawValue: 1 << 11)
+    
+    /// Strip authentication
+    /// From https://github.com/sindresorhus/normalize-url
+    static let stripAuthentication = Self(rawValue: 1 << 12)
+    
+    /// Strip protocol (only http and https)
+    /// From https://github.com/sindresorhus/normalize-url
+    static let stripProtocol = Self(rawValue: 1 << 13)
+    
+    /// Strip www
+    /// From https://github.com/sindresorhus/normalize-url
+    static let stripWWW = Self(rawValue: 1 << 14)
+    
+    /// Remove query parameters
+    /// From https://github.com/sindresorhus/normalize-url
+    static let removeQueryParameters = Self(rawValue: 1 << 15)
+    
+    /// Remove trailing slash
+    /// From https://github.com/sindresorhus/normalize-url
+    static let removeTrailingSlash = Self(rawValue: 1 << 16)
+    
+    /// Normalize protocol
+    /// From https://github.com/sindresorhus/normalize-url
+    static let normalizeProtocol = Self(rawValue: 1 << 17)
+}
+
 public extension URLComponents {
     struct Normalization: OptionSet {
         
@@ -20,91 +107,6 @@ public extension URLComponents {
         public init(rawValue: Int) {
             self.rawValue = rawValue
         }
-        
-        /// Normalizations that preserve semantics:
-        
-        /// Converting percent-encoded triplets to uppercase.
-        /// Decoding percent-encoded triplets of unreserved characters.
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let percentEncodings = Self(rawValue: 1 << 0)
-        
-        /// Converting the scheme and host to lowercase.
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let lowercaseSchemeAndHost = Self(rawValue: 1 << 1)
-        
-        /// Removing dot-segments.
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let removeDotSegments = Self(rawValue: 1 << 2)
-        
-        /// Converting an empty path to a "/" path.
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let convertEmptyPath = Self(rawValue: 1 << 3)
-        
-        /// Removing the default port.
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let removeDefaultPort = Self(rawValue: 1 << 4)
-        
-        
-        /// Normalizations that usually preserve semantics:
-        
-        /// Adding a trailing "/" to a non-empty path.
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let addTrailingSlash = Self(rawValue: 1 << 5)
-        
-        
-        /// Normalizations that change semantics:
-        
-        /// Removing the fragment.
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let removeFragment = Self(rawValue: 1 << 6)
-        
-        /// Removing duplicate slashes.
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let removeDuplicateSlashes = Self(rawValue: 1 << 7)
-        
-        /// Sorting the query parameters.
-        /// Not guaranteed to be stable!
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let sortQueryParameters = Self(rawValue: 1 << 8)
-        
-        /// Removing the "?" when the query is empty.
-        /// From https://en.wikipedia.org/wiki/URI_normalization
-        static let removeEmptyQuery = Self(rawValue: 1 << 9)
-        
-
-        /// JS normalizations:
-        
-        /// Force HTTP
-        /// From https://github.com/sindresorhus/normalize-url
-        static let forceHTTP = Self(rawValue: 1 << 10)
-        
-        /// Force HTTP
-        /// From https://github.com/sindresorhus/normalize-url
-        static let forceHTTPS = Self(rawValue: 1 << 11)
-        
-        /// Strip authentication
-        /// From https://github.com/sindresorhus/normalize-url
-        static let stripAuthentication = Self(rawValue: 1 << 12)
-        
-        /// Strip protocol (only http and https)
-        /// From https://github.com/sindresorhus/normalize-url
-        static let stripProtocol = Self(rawValue: 1 << 13)
-        
-        /// Strip www
-        /// From https://github.com/sindresorhus/normalize-url
-        static let stripWWW = Self(rawValue: 1 << 14)
-        
-        /// Remove query parameters
-        /// From https://github.com/sindresorhus/normalize-url
-        static let removeQueryParameters = Self(rawValue: 1 << 15)
-        
-        /// Remove trailing slash
-        /// From https://github.com/sindresorhus/normalize-url
-        static let removeTrailingSlash = Self(rawValue: 1 << 16)
-        
-        /// Normalize protocol
-        /// From https://github.com/sindresorhus/normalize-url
-        static let normalizeProtocol = Self(rawValue: 1 << 17)
     }
     
     mutating func normalize(options: Normalization) {
